ca776c66b5aa63ef483ecc9f471e1996ff0571d3
Hákon Logi Bergsson <hakonlogibergsson@Hakons-MacBook-Air.local>
2022-03-03 18:18:59 +0000

    Test 2 is working well

--- sample.py
+++ sample.py
@@ -1,3 +1,6 @@
 def fizzbuzz(integer):
     if integer % 3 == 0 and integer % 5 == 0:
         return "FizzBuzz"
+
+    elif integer % 5 == 0:
+        return "Buzz"
--- test_sample.py
+++ test_sample.py
@@ -9,6 +9,10 @@ def test_mod_by_3_and_5():
     "fizzbuzz" """
     assert fizzbuzz(15) == "FizzBuzz"
 
+def test_mod_5():
+    """Tests if the fizzbuzz function returns "Buzz" when given an integer divisable by 5 """
+    assert fizzbuzz(10) == "Buzz"
+
 
 
 
