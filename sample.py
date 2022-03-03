72d7a2e7037a3dac7eaf604f0fa0fba74e5b0234
Hákon Logi Bergsson <hakonlogibergsson@Hakons-MacBook-Air.local>
2022-03-03 18:15:57 +0000

    Test 1 has been satisfied

---
+++ sample.py
@@ -0,0 +1,3 @@
+def fizzbuzz(integer):
+    if integer % 3 == 0 and integer % 5 == 0:
+        return "FizzBuzz"
---
+++ test_sample.py
@@ -0,0 +1,19 @@
+from sample import fizzbuzz
+import pytest 
+
+
+
+
+def test_mod_by_3_and_5():
+    """Tests if an integer divisible by 3 and 5 put into fizzbuzz will return the string 
+    "fizzbuzz" """
+    assert fizzbuzz(15) == "FizzBuzz"
+
+
+
+
+
+
+
+
+
